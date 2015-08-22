fa73a5b967af53d1e430e760bfc0079fffb15d12
Sam Rae <samuelrae@gmail.com>
2015-08-22 23:55:50 +0100

    added click event to bugs so that they dissapear. Score increases by one each time

--- js/evolution.js
+++ js/evolution.js
@@ -2,6 +2,8 @@ $(document).on('ready',function() {
 
 console.log('js linked');
 
+var bugCount=0;
+
 //generate random absolute position based on width and height of bug-field
 function randomHeight() {
   var fieldHeight = $('.field').height();
@@ -16,13 +18,41 @@ function randomWidth() {
 }
 
 //create 10 bugs with random positions with '.field'
-for (i=0;i<10;i++) {
-  var bug = $('<div id="'+i+'">');
+//??QUESTION: I wanted to have a 'bug' constructor that I could use here but it became confusing with jquery as jquery is essentially creating an array of objects(divs) that I then call using $/Jquery to add properties to. I couldn't fugure out how this would work in conjuction with a constructor function ( or even just defining the bugs as object literals)
+for (i=0;i<16;i++) {
+  makeBug(i,'blue')
+}
+
+for (i=17;i<=20;i++) {
+  makeBug(i,'green')
+}
+
+function makeBug(n, colour) {
+  var bug = $('<div id="bug'+n+'">');
   bug.addClass('bug');
   bug.css('top',randomHeight())
   bug.css('left',randomWidth())
-  $('.field').append(bug);
+  bug.css('background-color',colour)
   $('.field').append(bug);
 };
+
+//put click event on all bugs so that they dissapear when clicked
+$('.bug').on('click', function(){
+  eatBug(this);
+  addCount();
+});
+
+function eatBug(x){
+  $(x).addClass('eatenBug');
+}
+
+function addCount(){
+  bugCount++;
+  updateScore();
+}
+
+function updateScore(){
+  $('.infoBox').html('<p> Bug Count:'+bugCount+'</p>')
+}
   
 });
